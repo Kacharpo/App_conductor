1dc74c12e693c5d59a26872b1e415b251c89da42
Carlos Alfonso Barrón Rivera <carloslego.br@gmail.com>
2021-07-06 02:20:33 -0500

    primer commit, splash

---
+++ app/src/main/java/com/example/app_conductor/Registro_conductor.java
@@ -0,0 +1,14 @@
+package com.example.app_conductor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Registro_conductor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro_conductor);
+    }
+}
