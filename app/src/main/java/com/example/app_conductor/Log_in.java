aa57e3e098fc4f12e0c9a822af18b7523ca27049
Carlos Alfonso Barrón Rivera <carloslego.br@gmail.com>
2021-07-09 02:41:52 -0500

    se integra registro conductor y pasar entre pantallas

--- app/src/main/java/com/example/app_conductor/Log_in.java
+++ app/src/main/java/com/example/app_conductor/Log_in.java
@@ -2,7 +2,9 @@ package com.example.app_conductor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Log_in extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class Log_in extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+
+    }
+
+    public void ingresar(View view){
+        Intent i = new Intent(Log_in.this, Registro_conductor.class);
+        startActivity(i);
+        finish();
     }
 }
