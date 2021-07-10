900cdf0ee658c7d74cb2beecc481e22ebf613149
Carlos Alfonso Barrón Rivera <carloslego.br@gmail.com>
2021-07-10 00:47:03 -0500

    se agrega nuevo logo y correciones de estilos

--- app/src/main/java/com/example/app_conductor/Mensaje.java
+++ app/src/main/java/com/example/app_conductor/Mensaje.java
@@ -2,7 +2,9 @@ package com.example.app_conductor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Mensaje extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class Mensaje extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mensaje);
+    }
+    public void avanzar(View view){
+        Intent i = new Intent(Mensaje.this, Log_in.class);
+        startActivity(i);
+        finish();
     }
 }
--- app/src/main/java/com/example/app_conductor/Registro_conductor.java
+++ app/src/main/java/com/example/app_conductor/Registro_conductor.java
@@ -2,7 +2,9 @@ package com.example.app_conductor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Registro_conductor extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class Registro_conductor extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro_conductor);
+    }
+    public void avanzar(View view){
+        Intent i = new Intent(Registro_conductor.this, Mensaje.class);
+        startActivity(i);
+        finish();
     }
 }
